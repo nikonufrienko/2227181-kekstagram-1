36b9a47810eb54abf24eb685e5c103332dc1c6bd
nikonufrienko <nikonufrienko@gmail.com>
2022-12-07 20:19:03 +0300

    task8 2

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const submitButton = document.querySelector('.img-upload__submit');
 
 function closeImgUploadOverlay(){
   imgUploadOverlay.classList.add('hidden');
@@ -8,12 +9,34 @@ function isImgUploadOverlayShowed() {
   return !imgUploadOverlay.classList.contains('hidden');
 }
 
+function blockSubmitButton() {
+  if(!submitButton.hasAttribute('disabled')) {
+    submitButton.toggleAttribute('disabled');
+  }
+}
+
+function unblockSubmitButton() {
+  if(submitButton.hasAttribute('disabled')) {
+    submitButton.toggleAttribute('disabled');
+  }
+}
+
 function initForm() {
   const fileInput = document.querySelector('#upload-file');
-
+  const form = document.querySelector('.img-upload__form');
+  form.addEventListener('submit', (evt)=>{
+    evt.preventDefault();
+    blockSubmitButton();
+    //...
+    unblockSubmitButton();
+    form.reset();
+    closeImgUploadOverlay();
+  });
   fileInput.addEventListener('change', () => {
     imgUploadOverlay.classList.remove('hidden');
   });
+
+
 
   const closeButton = document.querySelector('.img-upload__cancel');
 
